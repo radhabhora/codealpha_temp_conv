fc575c86a6c2c0296fd4a09fa684b875cd282877
radhabhora <144038772+radhabhora@users.noreply.github.com>
2023-09-04 20:57:35 +0530

    First Commit for temp web ite
    This web project are the celecius into fehranite and frhranite into Celecius

---
+++ script.js
@@ -0,0 +1,38 @@
+console.log('Welcome to 🌡️ Temperature Converter');
+
+// tempLoad();
+
+const calculateTemp = () => {
+    const numberTemp = document.getElementById('temp').value;
+    // console.log(numberTemp);
+
+    const tempSelected = document.querySelector('#temp_diff');
+    const valueTemp = temp_diff.options[tempSelected.selectedIndex].value;
+    // console.log(valeTemp);
+
+
+    // Convert temperature from Celcius to Fahrenheit
+    const celTOfah = (cel) => {
+        let fahrenheit = (cel * (9 / 5) + 32);
+        return fahrenheit;
+    }
+
+    // Convert temperature from Fahrenheit to Celsius
+    const fahTOcel = (fehr) => {
+        let celsius = ((fehr - 32) * 5 / 9);
+        return celsius;
+    }
+
+    let result;
+    if (valueTemp == "cel") {
+        result = celTOfah(numberTemp);
+        document.getElementById('resultContainer').innerHTML = `${result}°Fahrenheit`;
+    } else {
+        result = fahTOcel(numberTemp);
+        document.getElementById('resultContainer').innerHTML = `${result}°Celsius`;
+    }
+
+    // setTimeout(() => {
+    //     window.location.reload();
+    // }, 1500);
+}
